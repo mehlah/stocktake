b6b7412d1e8b707a424f1d54e06bc266ac3364f3
Mehdi Lahmam B <mehdi@lahmam.com>
2011-01-24 22:36:00 +0100

    Add an html template for product details

--- shows/product.js
+++ shows/product.js
@@ -1,16 +1,11 @@
 function (doc, req) {
-
-	var html = "<div id='product_page' data-role='page'>"+
-					"<div data-role='header'>"+
-            			"<h2 id='product_name'>"+ doc.name +"</h2>"+
-					"</div>"+
-					"<div id='product_details' data-role='content'>"+
-						"<p>" + doc.type + "</p>" +
-	                    "<p>" + doc.description + "</p>"+
-	                    "<p>" + doc.price + "</p>"+
-	                "</div>"+
-					"<div data-role='footer' data-position='fixed'><h4>Made on a couch</h4></div>"+
-				"</div>";
-				
-	return html;
+	var Mustache = require("vendor/couchapp/lib/mustache");
+	var stash = {
+	    name: doc.name,
+	    type : doc.type,
+	    description: doc.description,
+	    price: doc.price,
+	    document: doc._id
+	};
+	return Mustache.to_html(this.templates.product, stash);
 }
